6cd41cc09a0f4f76ff4ec8bae63a530e51fb9e98
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2015-05-03 18:28:25 +0200

    Adding basic feeding scripts

--- scritps/feed-ffmpeg.sh
+++ scritps/feed-ffmpeg.sh
@@ -1,1 +1,4 @@
-ffmpeg -f alsa -i default -f video4linux2 -s 640x480 -r 30 -i /dev/video0 -f avi - | ffmpeg2theora -a 0 -v 5 -f avi -x 640 -y 480 -o /dev/stdout - | oggfwd 150.214.150.68 4551 1qaz /$1.ogg
+# $1 the channel name
+# $2 the icecast password
+# $3 the icecast port
+ffmpeg -f alsa -i default -f video4linux2 -s 640x480 -r 30 -i /dev/video0 -f avi - | ffmpeg2theora -a 0 -v 5 -f avi -x 640 -y 480 -o /dev/stdout - | oggfwd 150.214.150.68 $3 $2 /$1.ogg
