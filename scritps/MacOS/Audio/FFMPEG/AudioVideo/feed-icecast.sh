c1ca8a0df67f870cc01e48d95e34783fdb51f847
vicente-gonzalez-ruiz <vicente.gonzalez.ruiz@gmail.com>
2015-09-28 00:31:49 +0200

    FFPEG on MacOS works

---
+++ scritps/MacOS/Audio/FFMPEG/AudioVideo/feed-icecast.sh
@@ -0,0 +1,11 @@
+# $1 the channel name
+# $2 the icecast password
+# $3 the icecast port
+
+# See:
+# ffmpeg -f avfoundation -list_devices true -i ""
+
+# This works:
+# ffmpeg -f avfoundation -pixel_format yuyv422 -r "25" -video_device_index 0 -i ":1" -y /tmp/1.avi
+
+ffmpeg -f avfoundation -pixel_format yuyv422 -r "25" -video_device_index 0 -i ":1" -c:a libmp3lame -b:a 128k -legacy_icecast 1 -content_type audio/mpeg -ice_name "DemoStream" -f mp3 icecast://source:$2@150.214.150.68:$3/$1.ogg
